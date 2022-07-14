ba0a8831bb952b888d99437c8d142ec96b450107
Valerii Voronkov <scorpirat@gmail.com>
2022-07-14 16:20:17 +0300

    Execute task for fs->list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,26 @@
+import * as fs from 'fs/promises';
+
+/**
+ * implement function that prints all array of filenames from files folder into console
+ * (if files folder doesn't exists Error with message FS operation failed must be thrown)
+ */
 export const list = async () => {
     // Write your code here 
+
+    try {
+        await fs.stat(process.cwd() + "/src/fs/files/");
+    } catch (error) {
+        console.log("FS operation failed must be thrown");
+        return;
+    }
+
+    try {
+        const res = await fs.readdir(process.cwd() + "/src/fs/files/");
+
+        for (const item of res) {
+            console.log(item);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
