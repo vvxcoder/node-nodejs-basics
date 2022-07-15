36054ff8526055fc9fa8a4bc242bc125902a2d95
Valerii Voronkov <scorpirat@gmail.com>
2022-07-15 09:06:33 +0300

    Execute task for fs->read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,24 @@
+import * as fs from 'fs/promises';
+
+/**
+ * implement function that prints content of the fileToRead.txt into
+ * console (if there's no file fileToRead.txt Error with message FS
+ * operation failed must be thrown)
+ */
 export const read = async () => {
     // Write your code here 
+
+    try {
+        await fs.stat(process.cwd() + "/src/fs/files/fileToRead.txt");
+    } catch (error) {
+        console.log("FS operation failed must be thrown");
+        return;
+    }
+
+    try {
+        const res = await fs.readFile(process.cwd() + "/src/fs/files/fileToRead.txt", "utf8");
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
 };
