b46214be0862ea0458762b2322c117f4731b54be
Valerii Voronkov <scorpirat@gmail.com>
2022-07-16 20:40:00 +0300

    Execute task cli->env

--- src/cli/env.js
+++ src/cli/env.js
@@ -4,10 +4,9 @@ import * as fs from 'fs/promises';
  * implement function that parses environment variables with prefix
  * RSS_ and prints them to the console in the format
  * RSS_name1=value1; RSS_name2=value2
+ * call RSS_name1=33 RSS_name2=66 node src/cli/env.js
  */
-const parseEnv = () => {
+export const parseEnv = () => {
     // Write your code here 
-    console.log(process.env);
+    console.log(`RSS_name1=${process.env.RSS_name1}; RSS_name2=${process.env.RSS_name2}`);
 };
-
-parseEnv();
