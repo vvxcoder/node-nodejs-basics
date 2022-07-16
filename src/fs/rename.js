cf97eb691e4a0c03c91759ef2084294dcb1829c1
Valerii Voronkov <scorpirat@gmail.com>
2022-07-16 13:40:15 +0300

    Execute task fs->rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,26 @@
+import * as fs from 'fs/promises';
+/**
+ * implement function that renames file wrongFilename.txt to properFilename with
+ * extension .md (if there's no file wrongFilename.txt or properFilename.md
+ * already exists Error with message FS operation failed must be thrown)
+ */
 export const rename = async () => {
     // Write your code here 
+    try {
+        await fs.stat(process.cwd() + "/src/fs/files/wrongFilename.txt");
+    } catch (error) {
+        console.log("FS operation failed must be thrown");
+    }
+    try {
+        await fs.stat(process.cwd() + "/src/fs/files/properFilename.md");
+        console.log("FS operation failed must be thrown");
+    } catch (error) {
+        //
+    }
+
+    try {
+        await fs.rename(process.cwd() + "/src/fs/files/wrongFilename.txt", process.cwd() + "/src/fs/files/properFilename.md");
+    } catch (error) {
+        console.log(error);
+    }
 };
